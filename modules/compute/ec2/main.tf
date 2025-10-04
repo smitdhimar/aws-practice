cd880132f0731c89a6409653ea97da4ce16803b6
smit <smitdhimar61@gmail.com>
2025-10-05 00:33:52 +0530

    feat: PEM key to local

--- modules/compute/ec2/main.tf
+++ modules/compute/ec2/main.tf
@@ -1,8 +1,28 @@
-resource "aws_instance" "demo_instance" {
-    ami = var.demo_instance_ami
-    instance_type = var.demo_instance_type
+# key pairs
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "ec2_key_pair"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+resource "local_file" "ec2_pem_keys" {
+  content         = tls_private_key.my_key.private_key_pem
+  file_permission = "0400"
+  filename        = "${path.module}/ec2_pem_keys.pem"
+}
 
-    tags = {
-        Name = "${var.demo_instance_prefix}-${var.project_name}-${var.environment}"
-    }
+# security groups
+
+
+# instances
+# demo instance 
+resource "aws_instance" "demo_instance" {
+  ami           = var.demo_instance_ami
+  instance_type = var.demo_instance_type
+
+  tags = {
+    Name = "${var.demo_instance_prefix}-${var.project_name}-${var.environment}"
+  }
 }
