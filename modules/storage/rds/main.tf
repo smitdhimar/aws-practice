27fe85b9500dd73a5e2beea995d642eee9a4dc02
smit <smitdhimar61@gmail.com>
2025-10-08 11:50:51 +0530

    fix(rds): change the configuration of publically accessible flag

--- modules/storage/rds/main.tf
+++ modules/storage/rds/main.tf
@@ -3,11 +3,12 @@ resource "aws_db_instance" "demo_db" {
   engine                 = "mysql"
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
-  identifier             = var.demo_db_identifier
+  identifier             = "${var.demo_db_identifier}-${var.project_name}-${var.environment}"
   username               = var.demo_db_username
   password               = var.demo_db_password
   parameter_group_name   = "default.mysql8.0"
   skip_final_snapshot    = true
+  publicly_accessible    = true
   vpc_security_group_ids = [var.security_group_id]
 
   tags = {
